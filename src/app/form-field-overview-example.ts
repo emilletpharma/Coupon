1bd4dfa28d21f24a979b44035e1b7996ae1e4afc
emilletpharma <eric.millet@lacooperativewelcoop.com>
2019-12-18 14:47:52 +0100

    1 file modified, 1 added, 1 deleted

--- src/app/form-field-overview-example.ts
+++ src/app/form-field-overview-example.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'form-field-overview-example',
   templateUrl: 'form-field-overview-example.html',
-  styleUrls: ['form-field-overview-example.css'],
+  styleUrls: ['form-field-overview-example.scss'],
 })
 export class FormFieldOverviewExample {}
 
